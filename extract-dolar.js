b2cf7a58d54c48b56b0869353de75f06dba77e69
yoinerdeyhan@gmail.com <yoinerdeyhan@gmail.com>
2025-01-05 13:58:50 -0500

    🔒️ [CA] validación de certificados ignorada

--- extract-dolar.js
+++ extract-dolar.js
@@ -16,27 +16,27 @@ const options = {
   ca: fs.readFileSync('/etc/letsencrypt/live/facturacion.amaxoniaerp.com/chain.pem'),
 };
 
+const https = require('https');
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 app.get('/api/dolar', async (req, res) => {
   try {
     console.log('Iniciando solicitud a https://www.bcv.org.ve/');
-    
-    const response = await axios.get('https://www.bcv.org.ve/');
-    console.log('Solicitud completada. Estado:', response.status);
 
-    // Verificar si se obtuvo el HTML
-    console.log('HTML recuperado:', response.data.slice(0, 500)); // Muestra los primeros 500 caracteres del HTML
+    const response = await axios.get('https://www.bcv.org.ve/', { httpsAgent: agent });
+    console.log('Solicitud completada. Estado:', response.status);
 
     const $ = cheerio.load(response.data);
     const dolarElement = $('#dolar .col-sm-6.col-xs-6.centrado strong');
-    console.log('Elemento encontrado con el selector:', dolarElement.html()); // Muestra el contenido del elemento encontrado
+    console.log('Elemento encontrado con el selector:', dolarElement.html());
 
     const dolarValueRaw = dolarElement.text().trim();
     console.log('Valor bruto extraído del HTML:', dolarValueRaw);
 
     if (dolarValueRaw) {
       const dolarValueProcessed = dolarValueRaw.replace(/\./g, '').replace(',', '.');
-      console.log('Valor procesado:', dolarValueProcessed);
-
       const dolarValue = parseFloat(dolarValueProcessed);
       console.log('Valor final como número:', dolarValue);
 
@@ -50,6 +50,7 @@ app.get('/api/dolar', async (req, res) => {
     return res.json({ success: false, dolar: -1 });
   }
 });
+
 
 https.createServer(options, app).listen(PORT, () => {
   console.log(`Servidor HTTPS ejecutándose en https://facturacion.amaxoniaerp.com:${PORT}`);
